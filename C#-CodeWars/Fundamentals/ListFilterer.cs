5693de0e7915810185ceeb4d37754fb880dfae21
Basurto, Victor {LALA} <victor.basurto@grupolala.com>
2023-09-03 20:38:49 -0600

    [ADD]

---
+++ C#-CodeWars/Fundamentals/ListFilterer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__CodeWars.Fundamentals
+{
+	internal class ListFilterer
+	{
+		public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems) => listOfItems.OfType<int>();
+	}
+}
